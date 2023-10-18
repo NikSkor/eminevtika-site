2546d23e3e26f004c874da9ea8cb94bc216a8329
NikSkor <mr-zerx@yandex.ru>
2023-10-18 05:55:11 +0500

    update

--- src/modules/showAndHide.js
+++ src/modules/showAndHide.js
@@ -2,15 +2,15 @@ const showAndHide = () => {
   const arrayList = document.querySelectorAll('.description__item');
   console.log('arrayList: ', arrayList);
   arrayList.forEach((item) => {
-    const btn = item.querySelector('.description__icon');
-    const text = item.querySelector('.description_text-block');
+    const btn = item.querySelector('.description__subheader');
+    const icon = item.querySelector('.description__icon');
+    const text = item.querySelector('.description__text-block');
 
     btn.addEventListener('click', (e) => {
       e.preventDefault();
-      text.classList.toggle('description_text-block_active');
+      text.classList.toggle('description__text-block_active');
+      icon.classList.toggle('description__icon_active');
     });
-    // console.log('item.children.children: ', item.children[0].children[1]);
-
   })
 };
 
